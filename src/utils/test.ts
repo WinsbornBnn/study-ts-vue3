1594b90b20176c39b43356b965027a17b6b08211
bnnwinsborn <15282922045@163.com>
2021-12-13 21:00:06 +0800

    搭建架子

--- src/utils/test.ts
+++ src/utils/test.ts
@@ -1,1 +1,9 @@
+//! 1、 我的第一个typescript程序
+let hello:any = 'Hello '
+hello += 'World!'
+console.log(hello)
+//! 2、TypeScript 基础类型
 
+export {
+  hello
+}
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="@/assets/logo.png" />
-    <h1>我的第一个TypeScript程序</h1>
+    <h2>我的第一个TypeScript程序</h2>
+    <h4>{{hello}}</h4>
   </div>
 </template>
 
 <script lang="ts">
+import { hello } from '@/utils/test'
 import { defineComponent, reactive, toRefs, onMounted } from 'vue'
 export default defineComponent({
   name: '',
@@ -23,7 +25,8 @@ export default defineComponent({
   },
   setup (props) {
     const state = reactive({
-      loading: false
+      loading: false,
+      hello: hello
     })
     onMounted(() => {
       console.log(props.aaaaa)
